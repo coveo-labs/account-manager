a6d58061861ebeaf465a037b19a8ca93923f9b3c
malpp <malpp@users.noreply.github.com>
2017-07-17 12:54:10 -0400

    unified return types

--- manager.py
+++ manager.py
@@ -9,6 +9,9 @@ class InvalidConfig(Exception):
 
 class Manager:
 
+	success = 'success'
+	error = 'error'
+
 	def __init__(self, push_url="pushdev.cloud.coveo.com", api_url="platformdev.cloud.coveo.com"):
 		config = yaml.safe_load(open("config.yml"))
 
@@ -49,10 +52,10 @@ class Manager:
 		return returnMessage
 
 	def generate_error(self, message):
-		return self.generate_return_message('error', message)
+		return self.generate_return_message(self.error, message)
 
 	def generate_success(self, message):
-		return self.generate_return_message('success', message)
+		return self.generate_return_message(self.success, message)
 
 	def get_push_headers(self):
 		headers = {
@@ -74,13 +77,13 @@ class Manager:
 		return "https://{}/v1/organizations/{}/sources/{}/documents?documentId={}".format(self.push_url, self.org_id, self.source_id, doc_id)
 
 	def get_user_url(self, username):
-		return """https://{}/rest/search/?numberOfResults=1&fieldsToInclude=%5B"%40username"%2C%20"%40password"%2C"%40salt"%5D&q=%40source%3Daccounts%20%40username%3D{}&access_token={}&organizationId={}&maximumAge=0""".format(self.api_url, username, self.coveo_api_key, self.org_id)
-
+		return """https://{}/rest/search/?numberOfResults=1&fieldsToInclude=%5B"%40username"%2C%20"%40password"%2C"%40salt"%2C"%40uniqueid"%5D&q=%40source%3Daccounts%20%40username%3D{}&access_token={}&organizationId={}&maximumAge=0""".format(self.api_url, username, self.coveo_api_key, self.org_id)
 
 	def get_user(self, username):
 		r = requests.get(self.get_user_url(username), headers=self.get_api_headers())
 		profile = json.loads(r.text)['results']
 		if len(profile) > 0:
+			profile[0]['raw']['uniqueid'] = profile[0]['uniqueId']
 			return self.generate_success(profile[0]['raw'])
 		else:
 			return self.generate_error("Could not find user - {}".format(r.status_code))
@@ -97,7 +100,7 @@ class Manager:
 
 	def add_user(self, username, password):
 		user = self.get_user(username)
-		if user['type'] == 'error':
+		if user['type'] == self.error:
 			self.put_user(username, password)
 			return self.generate_success('Added user')
 		else:
@@ -105,18 +108,18 @@ class Manager:
 
 	def validate_user(self, username, password):
 		user = self.get_user(username)
-		if user['type'] == 'error':
+		if user['type'] == self.error:
 			return user
 		else:
 			entered_password = self.generate_hashed_password(password, user['message']['salt'])
 			if(user['message']['password'] == entered_password):
-				return self.generate_success('match')
+				return user
 			else:
 				return self.generate_error('Wrong user/password')
 
 	def modify_password(self, username, current_password, new_password):
 		valid_user = self.validate_user(username, current_password)
-		if valid_user['type'] == 'success':
+		if valid_user['type'] == self.success:
 			self.put_user(username, new_password)
 			return self.generate_success('Changed password')
 		else:
--- test.py
+++ test.py
@@ -3,30 +3,39 @@ from time import sleep
 
 test = Manager()
 
-username = 'admin'
-oldpassword = 'admin1213'
-newpassword = 'admin'
+def change_password_test():
+    username = 'admin'
+    oldpassword = 'admin1213'
+    newpassword = 'admin'
 
-print test.validate_user(username, oldpassword)
+    print test.validate_user(username, oldpassword)
 
-while True:
-    user = test.get_user(username)
-    print user
-    if len(user) > 0 and user['type'] != 'error':
-        break
-    else:
-        sleep(5)
+    while True:
+        user = test.get_user(username)
+        print user
+        if len(user) > 0 and user['type'] != 'error':
+            break
+        else:
+            sleep(5)
 
-print "Valid password:"
-print test.validate_user(username, oldpassword)
+    print "Valid password:"
+    print test.validate_user(username, oldpassword)
 
-print "Mod password:"
-print test.modify_password(username, oldpassword, newpassword)
+    print "Mod password:"
+    print test.modify_password(username, oldpassword, newpassword)
 
-while True:
-    user = test.validate_user(username, newpassword)
-    print user
-    if len(user) > 0 and user['type'] != 'error':
-        break
-    else:
-        sleep(5)
+    while True:
+        user = test.validate_user(username, newpassword)
+        print user
+        if len(user) > 0 and user['type'] != 'error':
+            break
+        else:
+            sleep(5)
+
+def delete_user_test():
+    print test.delete_user('admin', 'admin')
+
+def add_user_test():
+    print test.add_user('admin', 'admin')
+
+print test.get_user('admin')
