60dc735f8087beb2ad02286876775d62d35b083a
malpp <malpp@users.noreply.github.com>
2017-07-18 10:20:29 -0400

    Removed useless code

--- test.py
+++ test.py
@@ -1,45 +1,6 @@
 from manager import Manager
 from time import sleep, time
-from timeit import default_timer as timer
 import unittest
-
-test = Manager()
-
-def change_password_test():
-    username = 'admin'
-    oldpassword = 'admin1213'
-    newpassword = 'admin'
-
-    print test.validate_user(username, oldpassword)
-
-    while True:
-        user = test.get_user(username)
-        print user
-        if len(user) > 0 and user['type'] != 'error':
-            break
-        else:
-            sleep(5)
-
-    print "Valid password:"
-    print test.validate_user(username, oldpassword)
-
-    print "Mod password:"
-    print test.modify_password(username, oldpassword, newpassword)
-
-    while True:
-        user = test.validate_user(username, newpassword)
-        print user
-        if len(user) > 0 and user['type'] != 'error':
-            break
-        else:
-            sleep(5)
-
-def add_user_test():
-    print test.add_user('admin1', 'admin')
-    start = timer()
-    print test.wait_until_user_created('admin1')
-    end = timer()
-    print(end - start) 
 
 class TestStringMethods(unittest.TestCase):
 
